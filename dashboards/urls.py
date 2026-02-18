d4233ccfd98511bae0111f92faa40ba2d3ae441e
BikashPrasad1 <prasadbikash013@gmail.com>
2026-02-18 23:39:52 +0530

    add user , remove user and edit user functionality

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+
+    path('users/', views.users, name='users'),
+    path('users/add', views.add_user, name='add_user'),
+    path('users/edit/<int:pk>', views.edit_user, name='edit_user'),
+    path('users/delete/<int:pk>', views.delete_user, name='delete_user'),
+
     path('categories/', views.categories, name='categories'),
     path('categories/add', views.add_category, name='add_category'),
     path('categories/edit/<int:pk>', views.edit_category, name='edit_category'),
